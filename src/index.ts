1a43d4174ffc1e378c31f7cf38d50ae64c322144
yr260625 <yr260625.learning@gmail.com>
2023-07-19 23:49:23 +0900

    [DEL] 不要ファイル削除

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { TabGroup } from 'src/tab-group';
 import { TableStructure } from 'src/table-structure';
-import { TreeStructure } from 'src/tree-structure';
 
 window.addEventListener('load', () => {
   const table = new TableStructure();
-  table.embedComponent();
-  const tree = new TreeStructure();
-  tree.embedComponent();
+  table.setupPage();
   const tabGroup = new TabGroup();
   tabGroup.setupPage();
 });
--- src/table-structure.ts
+++ src/table-structure.ts
@@ -1,14 +1,69 @@
-export class TableStructure {
-  constructor() {}
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
 
-  public async embedComponent() {
-    const response = await fetch('/templates/table.html');
-    const data = await response.text();
-    const targetDom = document.querySelector<HTMLElement>('#table-structure')!;
-    targetDom.innerHTML = data;
-  }
+export class TableStructure {
+  constructor(private users: Array<User> = []) {}
 
   public async setupPage() {
-    await this.embedComponent();
+    await this.createUserTable();
+  }
+
+  public async createUserTable() {
+    this.users = await this.fetchUsers();
+    this.users.forEach((user) => {
+      const clonedRow = document
+        .querySelector('.user-list-tempalate-row')!
+        .cloneNode(true) as HTMLElement;
+      const userForDisplay = this.getForDisplayUser(user);
+
+      for (const [key, value] of Object.entries(userForDisplay)) {
+        const colDom = clonedRow.querySelector(`[name=${key}]`);
+        if (colDom) {
+          clonedRow.querySelector(`[name=${key}]`)!.textContent = String(value);
+        }
+      }
+      const appendTargetDom = document.querySelector('.user-list-body');
+      if (appendTargetDom) {
+        clonedRow.classList.remove('user-list-tempalate-row');
+        appendTargetDom.appendChild(clonedRow);
+      }
+    });
+  }
+
+  private getForDisplayUser(user: User) {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      website: user.website,
+    };
+  }
+
+  public async fetchUsers() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const json = await response.json();
+    return json;
   }
 }
